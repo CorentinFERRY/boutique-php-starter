bb60d5cf834f46863eeddf67e1eff55da43435a9
Corentin FERRY <c.ferry78@laposte.net>
2026-01-05 13:58:55 +0100

    Jour 1 - Exercice 5
    - Concaténation et Interpolation
    - Affichage d'une même phrase de 3 manières différentes

---
+++ exercices/jour-01/texte.php
@@ -0,0 +1,14 @@
+<?php
+
+$brand = "Nike";
+$model = "Air Max";
+
+echo "Chaussures $brand $model <br>";
+echo 'Chaussures ' . $brand .' '. $model . '<br>';
+echo sprintf('Chaussures %s %s <br>',$brand,$model);
+
+$price = 99.99;
+echo "Prix : $price €";  // Prix : 99.99 €   Ici la variable est interprétée ("")
+echo 'Prix : $price €';  // Prix : $price €  Ici la variable N'EST PAS interprétée ('')
+
+?>
