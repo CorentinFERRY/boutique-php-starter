9fac9e4d98c49bb7a4f7d0ec8f2692c84479e53c
Corentin FERRY <c.ferry78@laposte.net>
2026-01-05 13:19:56 +0100

    Jour 1 - Deuxieme exercice
    - Déclaration des variables de 4 types
    - Vérification des types avec var_dump
    - Affichage d'une phrase avec les variables

---
+++ exercices/jour-01/produit.php
@@ -0,0 +1,22 @@
+<?php
+
+# Déclaration des variables
+$name = "Casquette";
+$price = 29.99;
+$stock = 2;
+$onSale = true;
+
+# Affiche le type de chaque variable 
+var_dump($name);
+echo "<br>";
+var_dump($price);
+echo "<br>";
+var_dump($stock);
+echo "<br>";
+var_dump($onSale);
+echo "<br>";
+
+# Affichage de la phrase "Le produit X coûte Y€"
+echo "Le produit $name coûte $price €";
+
+?>
