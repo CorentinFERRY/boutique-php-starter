45bf803dce28407bdb0e84ebfe95bdf3d2aa5a2d
Corentin FERRY <c.ferry78@laposte.net>
2026-01-05 13:44:18 +0100

    Jour 1 - Exercice 4
    - Ajout du fichier types.php
    - Affichage d'opérations avec des types mixtes
    - Ajout de commentaire pour l'affichage obtenu

---
+++ exercices/jour-01/types.php
@@ -0,0 +1,17 @@
+<?php
+$a = "5";
+$b = 3;
+$c = $a + $b;
+
+var_dump($a); // string(1) "5" 
+echo "<br>";
+var_dump($b); // int(3) 
+echo "<br>";
+var_dump($c); // int(8) 
+echo "<br>";
+
+$price = "29.99€";
+$result = $price + 10;
+var_dump($result); // float(39.989999999999995) 
+
+?>
