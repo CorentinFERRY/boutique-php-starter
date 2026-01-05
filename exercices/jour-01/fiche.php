162119b81843685abaa1beff50cbef08283ff6a9
Corentin FERRY <c.ferry78@laposte.net>
2026-01-05 14:25:38 +0100

    Jour 1 - Exercice 6
    - Intégration de PHP dans une page html
    - Création d'une fonction pour afficher En stock ou Rupture
    - Affichage des infos dans la page HTML

---
+++ exercices/jour-01/fiche.php
@@ -0,0 +1,27 @@
+<?php
+
+$name = "Casquette";
+$price = 29.99;
+$stock = 0;
+
+function isAvailable ($stock){
+    if($stock > 0)
+        echo "En stock";
+    else    
+        echo "Rupture";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $name ?></title>
+</head>
+<body>
+    <!-- À toi de jouer -->
+    <h1><?= $name ?></h1>
+    <p><?= $price ?>€</p>
+    <p><span><?= isAvailable($stock) ?></span></p>
+</body>
+</html>
