a53491bfef3389b19ac9cf8ee30ee35188270766
Corentin FERRY <c.ferry78@laposte.net>
2026-01-05 13:27:14 +0100

    Jour 1 - Exercice 3
    - Ajout de calculs.php
    - Affichage de résultats de calculs
    - Opérations mathématiques (+,*,/)
    - Concaténation avec le .

---
+++ exercices/jour-01/calculs.php
@@ -0,0 +1,14 @@
+<?php
+
+$priceExcludingTax = 100;
+$vat = 20;
+$quantity = 3;
+
+
+echo 'Le montant de la TVA est de ' . $priceExcludingTax / $vat . '€';
+echo "<br>";
+echo 'Le prix unitaire TTC est de ' .$priceExcludingTax+$priceExcludingTax/$vat.'€';
+echo "<br>";
+echo 'Le prix total pour la quantité commandée est de ' . ($priceExcludingTax + ($priceExcludingTax / $vat)) * $quantity . '€';
+
+?>
