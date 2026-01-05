586b70d6ec0894168d093ab5ec454c66a47e602b
Corentin FERRY <c.ferry78@laposte.net>
2026-01-05 16:23:43 +0100

    Jour 1 -  Intégration au projet -Création d'une fiche produit en php - Mettre cette fiche dans le dossier public

---
+++ public/produit.php
@@ -0,0 +1,53 @@
+<?php
+
+$name = "Casquette";
+$description = "Habbit de tête avec visière pour se protéger du soleil";
+$priceHT = 29.99;
+$vat = 20;
+$stock = 3;
+$priceTTC = calculedPriceTTC($priceHT, $vat);
+$discount = 15;
+$priceDiscount = calculedPriceTTC(calculedPriceDiscount($priceHT, $discount), $vat);
+
+
+function calculedPriceDiscount($priceHT, $discount)
+{
+    $result = $priceHT - $priceHT / $discount;
+    return $result;
+}
+
+function calculedPriceTTC($priceHT, $vat)
+{
+    $result = $priceHT / $vat + $priceHT;
+    return $result;
+}
+
+function isAvailable($stock)
+{
+    if ($stock > 0)
+        echo "En stock";
+    else
+        echo "Rupture";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title><?= $name ?></title>
+</head>
+
+<body>
+    <!-- À toi de jouer -->
+    <h1><?= $name ?></h1>
+    <p>
+        <?= $description ?><br>
+        <?= number_format($priceDiscount, 2, ',', ' ') . '€ '; ?><strike><?= sprintf("%01.2f €", $priceTTC) ?></strike><br>
+        <span><?= isAvailable($stock) . ' (' . $stock . ').' ?></span>
+    </p>
+</body>
+
+</html>
