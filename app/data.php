d1a7ce476dd0197be4449694f7c41143ffa6434f
Corentin FERRY <c.ferry78@laposte.net>
2026-01-06 14:54:44 +0100

    Jour 2 - Intégration
    - data.php avec mon tableau complexe
    - copie du fichier catalogue.html dans catalogue.php et adaptation pour recupérer les données dans notre tableaux
    - Bonus tri

---
+++ app/data.php
@@ -0,0 +1,137 @@
+<?php
+
+$products = [
+    [
+        "name" => "T-shirt Premium Bio",
+        "description" => "T-shirt en coton biologique de haute qualité. Coupe moderne et confortable, parfait pour un usage quotidien. Disponible en plusieurs coloris. Fabriqué de manière éthique et durable.",
+        "price" => 35.99,
+        "stock" => 45,
+        "images" => [
+            "https://static.nike.com/a/images/t_web_pdp_535_v2/f_auto/bb35906a-3643-4577-95d7-0650a0618e8a/U+NK+DF+CLUB+CAP+U+CB+MTSWSH+L.png",
+            "https://static.nike.com/a/images/t_web_pdp_535_v2/f_auto/46c954f9-289e-4706-894b-16cdc2b3c1dc/U+NK+DF+CLUB+CAP+U+CB+MTSWSH+L.png",
+            "https://static.nike.com/a/images/t_web_pdp_535_v2/f_auto/8c9a1630-557c-46f7-898b-112a362ea332/U+NK+DF+CLUB+CAP+U+CB+MTSWSH+L.png"
+        ],
+        "size" => ["S", "M", "L", "XL"],
+        "reviews" =>
+        [
+            [
+                "author" => "Alex Curtaud",
+                "rating" => 5,
+                "comment" => "Parfaite pour les randonnées en montagne et au soleil"
+            ],
+            [
+                "author" => "Corentin Ferry",
+                "rating" => 3,
+                "comment" => "Petit défaut de fabrication dans la fermeture à l'arrière mais très bon produit ! "
+            ],
+            [
+                "author" => "Ali",
+                "rating" => 5,
+                "comment" => "RAS"
+            ],
+        ]
+    ],
+    [
+        "name" => "Sneakers Urban",
+        "description" => "T-shirt classique Nike avec logo Swoosh",
+        "price" => 99.99,
+        "stock" => 3,
+        "images" => [
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f2e33f2e-0e2c-44e9-aa65-3db55fd37921/sportswear-swoosh-mens-t-shirt-MGJmFr.png",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/9ada93c7-a0eb-4f34-8b78-f8e88a6a0c9d/sportswear-swoosh-mens-t-shirt-MGJmFr.png",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/12dc6ca3-a13b-4bbe-addc-71f1b4cde9e4/sportswear-swoosh-mens-t-shirt-MGJmFr.png"
+        ],
+        "size" => ["S", "M", "L", "XL"],
+        "reviews" => [
+            [
+                "author" => "Lucas",
+                "rating" => 4,
+                "comment" => "Léger et confortable"
+            ],
+            [
+                "author" => "Emma",
+                "rating" => 5,
+                "comment" => "Parfait pour courir"
+            ]
+        ]
+    ],
+
+    [
+        "name" => "Casquette Vintage",
+        "description" => "Casquette Nike classique avec logo brodé",
+        "price" => 24.99,
+        "stock" => 28,
+        "images" => [
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c42589a7-1e64-40be-bd52-ad9b70180279/club-futura-cap-HmXB9p.png",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e4f2a42d-b71b-4b85-9a1a-e5e3c52b12fb/club-futura-cap-HmXB9p.png",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f1bb318a-04f2-470d-9f37-ecc29fbbdbc8/club-futura-cap-HmXB9p.png"
+        ],
+        "size" => ["One Size"],
+        "reviews" => [
+            [
+                "author" => "Nina",
+                "rating" => 5,
+                "comment" => "Très doux et bien coupé"
+            ]
+        ]
+    ],
+
+    [
+        "name" => "Jean Slim Stretch",
+        "description" => "Short léger Nike Dri-FIT pour sport et entraînement",
+        "price" => 79.99,
+        "stock" => 20,
+        "images" => [
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8cfa2823-e4cd-44d1-964b-fad03d0056e9/df-short-homme-Nike-Dri-FIT-L.png",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f66d1ba3-d1f5-4881-ae66-48f59b8dbb42/df-short-homme-Nike-Dri-FIT-L.png",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/0e0b27af-3f3f-4f7f-b05c-5da8b046e8e9/df-short-homme-Nike-Dri-FIT-L.png"
+        ],
+        "size" => ["S", "M", "L", "XL"],
+        "reviews" => [
+            [
+                "author" => "Maxime",
+                "rating" => 4,
+                "comment" => "Bon amorti"
+            ],
+            [
+                "author" => "Sarah",
+                "rating" => 5,
+                "comment" => "Très confortables"
+            ]
+        ],
+    ],
+    [
+        "name" => "Sac à dos Urbain",
+        "description" => "Sac à dos d’entraînement Nike Brasilia, spacieux et durable",
+        "price" => 59.99,
+        "stock" => 12,
+        "images" => [
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f7c55816-1314-4e63-9332-b0af23bd22ea/brasilia-training-backpack-XL5x6P.png",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e7167589-0bd5-4321-8b2f-c78492892b9d/brasilia-training-backpack-XL5x6P.png",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b0845fa7-9e07-4dd9-b75a-31791f5b3f62/brasilia-training-backpack-XL5x6P.png"
+        ],
+        "size" => ["One Size"],
+        "reviews" => [
+            [
+                "author" => "Yanis",
+                "rating" => 5,
+                "comment" => "Très agréable à porter"
+            ]
+        ]
+    ],
+
+    [
+        "name" => "Montre Classic",
+        "description" => "Sweat confort Nike avec capuche, idéal pour l’extérieur",
+        "price" => 89.99,
+        "stock" => 0,
+        "images" => [
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/827e69ad-5a97-4f7b-9873-cbea2f644873/sportswear-club-fleece-hoodie-M-8LRWr5.png",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1ede4e3e-ed19-48a3-9903-8a80f9739cc4/sportswear-club-fleece-hoodie-M-8LRWr5.png",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/3da2cd0b-8f8b-4824-b317-40cab439671b/sportswear-club-fleece-hoodie-M-8LRWr5.png"
+        ],
+        "size" => ["S", "M", "L", "XL"],
+        "reviews" => []
+    ]
+
+];
--- exercices/jour-02/catalogue.php
+++ exercices/jour-02/catalogue.php
@@ -35,3 +35,28 @@ echo $product[count($product)-1]["stock"].'<br>';       // 0
 $product[1]["stock"] += 10;
 echo $product[1]["stock"].'<br>';                       // 22
 
+// Bonus Fonctions de tri 
+print_r($product);
+echo "<br>";
+
+// Tri par ordre croissant tableau multidimension
+// Recupération de la colonne price de notre tableau
+$column = array_column($product,"price");
+var_dump($column);
+echo "<br>";
+
+// Tri par ordre croissant du tableau sur la colonne price (FLAGS SORT_ASC pour croissant SORT_DESC pour decroissant)
+array_multisort($column,SORT_ASC,$product);
+print_r($product);
+echo "<br>";
+
+// Tri par ordre alphabétique sur la 1ere clé du tableau ici "name"
+
+sort($product);
+print_r($product);
+echo "<br>";
+
+// Tri par ordre alphabétique du nom avec conservation de l'index 
+asort($product);
+print_r($product);
+echo "<br>"; 
