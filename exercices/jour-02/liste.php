16b1173e2089f141ca7712a746b2f377dad3bd85
Corentin FERRY <c.ferry78@laposte.net>
2026-01-06 09:52:40 +0100

    Jour 2 - Exercice 1 - Déclaration d'un tableau - Affichage d'une valeur d'un tableau - Ajout d'entrée dans le tableau - Suppression d'entrée dans la tableau - Affichage du tableau

---
+++ exercices/jour-02/liste.php
@@ -0,0 +1,21 @@
+<?php
+
+$groceries = ["Casquette", "Tee-shirt", "Lunette", "Jean", "Chaussures"];
+
+echo $groceries[0];
+echo "<br>";
+echo $groceries[count($groceries) - 1];
+echo "<br>";
+var_dump( count($groceries));
+echo "<br>";
+$groceries[] = "Pull";
+echo $groceries[count($groceries) - 1];
+echo "<br>";
+array_push($groceries,"Manteau");
+echo $groceries[count($groceries) - 1];
+echo "<br>";
+
+unset($groceries[2]);
+var_dump($groceries);
+
+?>
