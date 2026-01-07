7aad670bd3d8a94eefc509389eb4b6e7a1f9007e
Corentin FERRY <c.ferry78@laposte.net>
2026-01-07 15:46:47 +0100

    Jour 4 - Exercice 2 - Exemple de comparaison simple == et strict === - Compraison des résultats

---
+++ exercices/jour-04/comparaison.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = 0;
+$b = "";
+$c = null;
+$d = false;
+$e = "0";
+
+var_dump($a == $b);         // False  
+var_dump($a == $c);         // True 
+var_dump($a == $d);         // True
+var_dump($a == $e);         // True
+echo "<br><br>";
+
+var_dump($a === $b);        // False
+var_dump($a === $c);        // False
+var_dump($a === $d);        // False
+var_dump($a === $e);        // False
+echo "<br><br>";
+
+// Le seul cas où avec == on a le bon résultat c'est la comparaison de $a et $b 
