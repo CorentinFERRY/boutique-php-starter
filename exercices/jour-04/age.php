3fc6fb6e10dbf48e53660d60d438c601bb36cf13
Corentin FERRY <c.ferry78@laposte.net>
2026-01-07 15:45:24 +0100

    Jour 4 - Exercice 1 -Affichage d'un message en fonction d'une variable

---
+++ exercices/jour-04/age.php
@@ -0,0 +1,15 @@
+<?php 
+
+$age = 0;
+
+if ($age < 0)
+    echo "Vous avez saisie un age négatif !";
+elseif ($age < 18)
+    echo "minor";
+elseif ($age <= 25)
+    echo "Young adult";
+elseif ($age <=64)
+    echo "Adult";
+else    
+    echo "Senior";
+
