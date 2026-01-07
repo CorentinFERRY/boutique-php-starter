e7116f287c276fff61b573261092cbba1eec702c
Corentin FERRY <c.ferry78@laposte.net>
2026-01-07 09:50:15 +0100

    Jour 3 - Exercice 4 -Boucle for simple

---
+++ exercices/jour-03/for.php
@@ -0,0 +1,21 @@
+<?php
+
+for ($i = 1 ; $i <= 10 ; $i++){
+    echo $i;
+    echo "<br>";
+}
+
+for ($i = 2 ; $i <= 20 ; $i+=2){
+    echo $i;
+    echo "<br>";
+}
+
+for ($i = 10 ; $i >= 0 ; $i--){
+    echo $i;
+    echo "<br>";
+}
+
+for ($i = 1 ; $i <= 10 ; $i++){
+    echo $i*7;
+    echo "<br>";
+}
