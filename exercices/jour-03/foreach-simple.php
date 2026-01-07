7a5f21f186555a839e45a29c346b4a1ab5c8abd3
Corentin FERRY <c.ferry78@laposte.net>
2026-01-07 09:16:46 +0100

    Jour 3 - Exercice 1 - Premier foreach simple - Parcours d'un tableau et affichage des valeurs

---
+++ exercices/jour-03/foreach-simple.php
@@ -0,0 +1,11 @@
+<?php
+
+$firstNames = ["Corentin","Bryan","Ali","Alex","Charlotte"];
+$i = 1;
+
+echo "<ul>";
+foreach ($firstNames as $firstName){
+    echo "<li> $i. $firstName </li>";
+    $i++;
+}
+echo "</ul>";
