f512cf6c102ed3605ddaffc15675cfabf99c1929
Corentin FERRY <c.ferry78@laposte.net>
2026-01-07 09:38:56 +0100

    Jour 3 - Exercice 3
    - Reprise de la liste de produits dans un tableau multidimensionnel
    - Affichage des produits dans une structure HTML à l'aide d'une boucle

---
+++ exercices/jour-03/liste-produits.php
@@ -0,0 +1,37 @@
+<?php
+
+$products = [
+    [
+        "name" => "Casquette",
+        "price" => 29.99,
+        "stock" => 3,
+    ],
+    [
+        "name" => "Lunettes",
+        "price" => 165,
+        "stock" => 12,
+    ],
+    [
+        "name" => "T-shirt",
+        "price" => 49.99,
+        "stock" => 2,
+    ],
+    [
+        "name" => "Jean",
+        "price" => 119.99,
+        "stock" => 9,
+    ],
+    [
+        "name" => "Pull",
+        "price" => 59.99,
+        "stock" => 0,
+    ]
+];
+
+foreach ($products as $product){
+    echo '<article>
+            <h3>'.$product["name"].'</h3>
+            <p class="prix">'.$product["price"].' €</p>
+            <p class="stock">Stock : '.$product["stock"].'</p>
+         </article>';
+}
