29c30d849f14c0a61661ec28e8dee79e05136b66
CorentinFERRY <c.ferry78@laposte.net>
2026-01-08 15:23:21 +0000

    Jour 5 - Exercice 3 -Creation d'une fonction avec des paramètres par défaut - Différents appels à cette fonction

---
+++ exercices/jour-05/defaut.php
@@ -0,0 +1,12 @@
+<?php
+
+function formatPrice ($amount,$currency = '€', $decimals = 2 ){
+    return number_format($amount,$decimals,',').$currency;
+}
+
+echo formatPrice(99.999);
+echo "<br>";
+echo formatPrice(99.999,"$");
+echo "<br>";
+echo formatPrice(99.999,"€",0);
+echo "<br>";
