ad833dceeac8303de6eefeb7f4e9dd4146b0a5ac
CorentinFERRY <c.ferry78@laposte.net>
2026-01-09 09:30:25 +0000

    Jour 5 - Exercice 6 - Création d'un fichier helpers.php contenant nos fonctions utiles - Inclusion de notre helpers.php dans page.php avec require_once -Utilisation des fonctions de helpers.php dans page.php

---
+++ exercices/jour-05/page.php
@@ -0,0 +1,23 @@
+<?php
+require_once "helpers.php"; // Permet d'inclure le fichier php avec nos fonctions et les utiliser dans notre page
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?= displayBadge(" 🔥 PROMO") ?>
+    <?= displayBadge("NOUVEAU","green") ?>
+    <?= displayBadge("RUPTURE","red") ?>
+    <?= displayPrice(100,10) ?>
+    <?= displayPrice(90) ?>
+    <?= displayStock(0) ?>
+    <?= displayStock(4) ?>
+    <?= displayStock(15) ?>
+</body>
+</html>
