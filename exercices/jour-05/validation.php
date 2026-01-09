37a3efe87edae8e4b0884899829f3e7355359d2d
CorentinFERRY <c.ferry78@laposte.net>
2026-01-09 08:27:39 +0000

    Jour 5 - Exercice 4
    - Fin des fonctions de validation

--- exercices/jour-05/validation.php
+++ exercices/jour-05/validation.php
@@ -1,23 +1,53 @@
-<?php 
+<?php
 
-function isInStock($stock){
-    if($stock > 0)
+function isInStock($stock)
+{
+    if ($stock > 0)
         return true;
     return false;
 }
 
-function isOnSale($discount){
-    if($discount > 0)
+function isOnSale($discount)
+{
+    if ($discount > 0)
         return true;
     return false;
 }
 
-function isNew($dateAdded){
+function isNew($dateAdded)
+{
     $daysSince = (time() - strtotime($dateAdded));
-    if($daysSince < 720)
+    if ($daysSince < 2592000)
         return true;
     return false;
 }
+
+function canOrder($stock, $quantity)
+{
+    if ($stock >= $quantity)
+        return true;
+    return false;
+}
+
+var_dump(isInStock(5));
 echo "<br>";
-var_dump(isNew("2026-01-07"));
-var_dump(isNew("2024-01-06"));
+var_dump(isInStock(0));
+echo "<br>";
+var_dump(isInStock(-5));
+echo "<br>";
+var_dump(isOnSale(5));
+echo "<br>";
+var_dump(isOnSale(0));
+echo "<br>";
+var_dump(isOnSale(-5));
+echo "<br>";
+var_dump(isNew("2026-01-10"));
+echo "<br>";
+var_dump(isNew("2025-12-10"));
+echo "<br>";
+var_dump(canOrder(5, 2));
+echo "<br>";
+var_dump(canOrder(0, 8));
+echo "<br>";
+var_dump(canOrder(3, 4));
+echo "<br>";
