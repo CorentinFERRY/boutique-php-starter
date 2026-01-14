a2137104134f968edb658ec38a1f6fe3517472c8
Corentin FERRY <c.ferry78@laposte.net>
2026-01-14 11:33:57 +0100

    Jour 7 - Exercice 4
    - Connexion à la BDD avec gestion d'erreurs
    - Preparation et execution de la requete SQL SELECT
    - Récupération dans un tableau associatif à l'aide de fetchAll(PDO::FETCH_ASSOC)

---
+++ exercices/jour-07/liste-produits.php
@@ -0,0 +1,24 @@
+<?php
+
+try {
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=boutique;charset=utf8mb4",
+        "dev",
+        "dev",
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] // Gestion d'erreurs
+    );
+// Permet de récupérer les erreurs et de les afficher si il y en a
+} catch(PDOException $e){
+    echo "❌ Erreur : " . $e->getMessage();
+};
+
+
+
+$cmd = $pdo->prepare("SELECT * FROM products");
+$cmd->execute();
+
+$products = $cmd->fetchAll(PDO::FETCH_ASSOC); // PDO::FETCH_ASSOC permet de retourner les données dans un tableau associatif correspondant à la données de la BDD
+print_r($products);
+
+
+
