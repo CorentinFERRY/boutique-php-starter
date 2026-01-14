262d757c8542aa41a9106c2b498c38d0798eb24e
Corentin FERRY <c.ferry78@laposte.net>
2026-01-14 16:02:08 +0100

    Jour 8 - Exercice 2
    Utilisation du constructeur
    paramètre date par défaut
    Utilisation de la classe DateTimeImmutable pour avoir la date en fonction du jour

---
+++ exercices/jour-08/User.php
@@ -0,0 +1,27 @@
+<?php
+
+Class User {
+
+    public string $name;
+    public string $email;
+    public DateTimeImmutable $registrationDate;
+
+    public function __construct(string $name,string $email,DateTimeImmutable $registrationDate = new DateTimeImmutable('now'))
+    {     
+        $this->name = $name;
+        $this->email = $email;
+        $this->registrationDate = $registrationDate;
+    }
+
+    public function isNewMember() : bool {
+        $today = new DateTimeImmutable('now');
+        $interval = $this->registrationDate->diff($today); 
+        return $interval->days > 30;
+    }
+}
+
+$Coco = new User("Corentin","testestest",new DateTimeImmutable('2020-10-15'));
+$Alex = new User("Alex","testestest");
+
+var_dump($Alex->isNewMember());
+var_dump($Coco->isNewMember());
