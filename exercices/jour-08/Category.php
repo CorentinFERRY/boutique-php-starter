71acde89d514d9e3743b472ae20cd24c96fab177
Corentin FERRY <c.ferry78@laposte.net>
2026-01-15 09:54:26 +0100

    Jour 8 - Exercice 6
    Classe Category et retour du slug

---
+++ exercices/jour-08/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+class Category
+{
+
+    public function __construct(
+        public int $id,
+        public string $nom,
+        public string $description
+    ) {}
+
+    public function getSlug(): string
+    {
+
+        $slug = mb_strtolower($this->nom);
+        $slug = str_replace(' ', '-', $slug);
+        return $slug;
+    }
+}
+
+
+$CategoryTest = new Category(1, "AEFEZFÉ àezfezfzf", "ceci est un test");
+$CategoryTest2 = new Category(2, "Accessoires à sac", "ceci est un test");
+$CategoryTest3 = new Category(3, "Machine à coudre", "ceci est un test");
+
+echo $CategoryTest->getSlug() . "<br>";
+echo $CategoryTest2->getSlug() . "<br>";
+echo $CategoryTest3->getSlug() . "<br>";
