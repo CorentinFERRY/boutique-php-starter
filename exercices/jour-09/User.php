b861b10a45a6f1c7091eeaa799075dee232581ea
Corentin FERRY <c.ferry78@laposte.net>
2026-01-15 13:33:53 +0100

    Jour 9 - Exercice 4
    Creation des classes User et Address
    
    Ajout des méthodes des classes

---
+++ exercices/jour-09/User.php
@@ -0,0 +1,58 @@
+<?php
+
+class User
+{
+
+    /** @var Address[] */
+    private array $address = [];
+
+    public function __construct(
+        private string $name,
+        private string $mail,
+        private DateTimeImmutable $dateInscription = new DateTimeImmutable('now'),
+    ) {}
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getMail(): string
+    {
+        return $this->mail;
+    }
+
+    public function addAddress(Address $address, int $id) : void
+    {
+        if (!isset($this->address[$id])) {
+            $this->address[$id] = $address; 
+        }
+    }
+
+    public function getAddresses() : array
+    {
+        return $this->address;
+    }
+
+    public function getDefaultAddress () : string
+    {
+        return $this->address[0]->getAddress();
+    }
+}
+
+
+class Address
+{
+
+    public function __construct(
+        private int $numero,
+        private string $street,
+        private string $city,
+        private int $postalCode,
+        private string $country
+    ) {}
+
+    public function getAddress () : string {
+        return "$this->numero $this->street $this->city $this->postalCode $this->country ";
+    }
+}
